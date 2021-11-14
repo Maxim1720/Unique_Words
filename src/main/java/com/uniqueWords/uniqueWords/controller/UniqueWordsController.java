c81d332f12b73fc4895cca5acdfd918ea58af542
Maxim Musiev <musiev_maksim@mail.ru>
2021-11-15 03:47:23 +0400

    upd realization

--- src/main/java/com/uniqueWords/uniqueWords/controller/UniqueWordsController.java
+++ src/main/java/com/uniqueWords/uniqueWords/controller/UniqueWordsController.java
@@ -1,33 +1,42 @@
 package com.uniqueWords.uniqueWords.controller;
 
 import com.uniqueWords.uniqueWords.entity.Word;
-import com.uniqueWords.uniqueWords.service.WordService;
-import com.uniqueWords.uniqueWords.correctObjects.URL;
+import com.uniqueWords.uniqueWords.service.MainService;
+import com.uniqueWords.uniqueWords.service.UrlService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.function.ServerResponse;
 
-import javax.validation.Valid;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.http.HttpResponse;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/unique_words")
 public class UniqueWordsController {
 
+    MainService mainService;
+
     @Autowired
-    private WordService wordService;
+    public UniqueWordsController(MainService mainService)
+    {
+        this.mainService = mainService;
+    }
 
     @GetMapping
-    public ResponseEntity get(@Valid @RequestBody URL url) {
+    public ResponseEntity get(@RequestParam String url) {
 
         ResponseEntity responseEntity;
         try {
-            List<Word> wordList = wordService.getAllByUrl(url.getUrl().toCharArray());
+            Set<Word> wordList = mainService.getWords(url);
             responseEntity = ResponseEntity.ok(wordList);
-        } catch (IOException e) {
-            responseEntity = ResponseEntity.badRequest().body("incorrect url");
+        } catch (IOException | IllegalArgumentException e) {
+            responseEntity = ResponseEntity.badRequest().body("incorrect url:\n" + e.getMessage());
         }
         return responseEntity;
     }
