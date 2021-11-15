f20f81da25bbab8495af5afe333e3fb9bcdaf690
Maxim Musiev <musiev_maksim@mail.ru>
2021-11-15 04:03:26 +0400

    rm unused imports

--- src/main/java/com/uniqueWords/uniqueWords/controller/UniqueWordsController.java
+++ src/main/java/com/uniqueWords/uniqueWords/controller/UniqueWordsController.java
@@ -2,18 +2,14 @@ package com.uniqueWords.uniqueWords.controller;
 
 import com.uniqueWords.uniqueWords.entity.Word;
 import com.uniqueWords.uniqueWords.service.MainService;
-import com.uniqueWords.uniqueWords.service.UrlService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.function.ServerResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.http.HttpResponse;
-import java.util.List;
 import java.util.Set;
 
 @Controller
