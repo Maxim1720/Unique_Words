0a07b258bbf74a14b661684f04553dc73a9e8f9c
Maxim Musiev <musiev_maksim@mail.ru>
2021-11-15 04:04:13 +0400

    rm unused imports

--- src/test/java/com/uniqueWords/uniqueWords/service/UrlServiceTest.java
+++ src/test/java/com/uniqueWords/uniqueWords/service/UrlServiceTest.java
@@ -1,13 +1,9 @@
 package com.uniqueWords.uniqueWords.service;
 
-import com.uniqueWords.uniqueWords.correctObjects.URL;
 import com.uniqueWords.uniqueWords.entity.Url;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class UrlServiceTest {
